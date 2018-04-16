6c17a54753e73bfe9b2966b4f485f1916adfd469
Dima Chaar <dimachaar1@gmail.com>
2018-04-16 14:29:55 -0400

    post discussion section

--- model.py
+++ model.py
@@ -4,14 +4,17 @@ from datetime import datetime
 
 GUESTBOOK_ENTRIES_FILE = "entries.json"
 entries = []
-
-def init(app):
-    global entries
+next_id=0
+def init():
+    global entries, next_id
     try:
-
         f = open(GUESTBOOK_ENTRIES_FILE)
         entries = json.loads(f.read())
         f.close()
+        # entries[0]['id']+1
+        for each_dict in entries:
+            if each_dict['id'] >next_id:
+                next_id = each_dict['id']+1
     except:
         print('Couldn\'t open', GUESTBOOK_ENTRIES_FILE)
         entries = []
@@ -21,11 +24,22 @@ def get_entries():
     return entries
 
 def add_entry(name, text):
-    global entries, GUESTBOOK_ENTRIES_FILE
+    global entries, GUESTBOOK_ENTRIES_FILE, next_id
     now = datetime.now()
     time_string = now.strftime("%b %d, %Y %-I:%M %p")
-    entry = {"author": name, "text": text, "timestamp": time_string}
+    entry = {"author": name, "text": text, "timestamp": time_string, "id":next_id}
     entries.insert(0, entry) ## add to front of list
+    try:
+        next_id +=1
+        f = open(GUESTBOOK_ENTRIES_FILE, "w")
+        dump_string = json.dumps(entries)
+        f.write(dump_string)
+        f.close()
+    except:
+        print("ERROR! Could not write entries to file.")
+
+def delete_entry(_id):
+    #delete an element from entries that corresponds to id and then
     try:
         f = open(GUESTBOOK_ENTRIES_FILE, "w")
         dump_string = json.dumps(entries)
